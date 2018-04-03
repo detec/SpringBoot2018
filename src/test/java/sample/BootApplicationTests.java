f246d78565e22bae4bb0f0f3dcec52a9ed079acf
Andrii Duplyk <andrii.duplyk@nitka.com>
2018-04-03 09:36:11 +0300

    Cleaned from temporary dependencies

--- src/test/java/sample/BootApplicationTests.java
+++ src/test/java/sample/BootApplicationTests.java
@@ -1,14 +1,11 @@
 package sample;
 
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @RunWith(JUnitPlatform.class)
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class BootApplicationTests {
 
